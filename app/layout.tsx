cf2877094a55386a41870a6268fb1278ed324c91
Precise Goals <elitesarthakamr@gmail.com>
2025-07-27 12:43:36 +0530

    changes to deployement and debugging bhai chalja please tere per padhta hu please please <3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Carex Ai | Falcons",
@@ -64,7 +54,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
           suppressHydrationWarning={true}
         >
           <Provider>{children}</Provider>
